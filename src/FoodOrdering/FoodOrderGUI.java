4d932d25a72785abd07e9de29801a440313bf3e6
Morningstar <francoplenos@gmail.com>
2022-12-17 20:07:01 +0800

    Rename FoodOrder components and Add FoodOrder main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,9 +2,27 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+    private JCheckBox cbPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cbBurger;
+    private JCheckBox cbFries;
+    private JCheckBox cbDrinks;
+    private JCheckBox cbTea;
+    private JCheckBox cbSundae;
+    private JRadioButton rbFive;
+    private JRadioButton rbTen;
+    private JRadioButton rbFifteen;
+
+    public static void main(String[] args) {
+        FoodOrderGUI food = new FoodOrderGUI();
+        food.setContentPane(food.panel1);
+        food.setTitle("Simple Calculator");
+        food.setBounds(400, 100, 500, 500);
+//        food.setSize(300,400);
+        food.setVisible(true);
+        food.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
