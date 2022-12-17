a79219ae25e80d3f9e926a6101e2cbdf9946cce3
Morningstar <francoplenos@gmail.com>
2022-12-17 20:25:58 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,9 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
@@ -15,6 +18,54 @@ public class FoodOrderGUI extends JFrame{
     private JRadioButton rbFive;
     private JRadioButton rbTen;
     private JRadioButton rbFifteen;
+
+    public FoodOrderGUI() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DecimalFormat d = new DecimalFormat("0.00");
+                float amount = 0;
+                float discount = 0;
+
+                if(cbPizza.isSelected()){
+                    amount += 100;
+                }
+                if(cbBurger.isSelected()){
+                    amount += 80;
+                }
+                if(cbFries.isSelected()){
+                    amount += 65;
+                }
+                if(cbDrinks.isSelected()){
+                    amount += 55;
+                }
+                if(cbTea.isSelected()){
+                    amount += 50;
+                }
+                if(cbSundae.isSelected()){
+                    amount += 40;
+                }
+
+                if(rbNone.isSelected()){
+                    JOptionPane.showMessageDialog(btnOrder, "The total price is: " + d.format(amount));
+                } else if(rbFive.isSelected()){
+                    discount = (float) (amount * 0.05);
+                    amount -= discount;
+                    JOptionPane.showMessageDialog(btnOrder, "The total price is: " + d.format(amount));
+                } else if(rbTen.isSelected()) {
+                    discount = (float) (amount * 0.10);
+                    amount -= discount;
+                    JOptionPane.showMessageDialog(btnOrder, "The total price is: " + d.format(amount));
+                } else if(rbFifteen.isSelected()) {
+                    discount = (float) (amount * 0.15);
+                    amount -= discount;
+                    JOptionPane.showMessageDialog(btnOrder, "The total price is: " + d.format(amount));
+                }
+
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         FoodOrderGUI food = new FoodOrderGUI();
